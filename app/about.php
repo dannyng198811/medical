17a08eab544b20fd10a2951801c69ce50a314a7e
dannyng198811 <danny.ng198811@yahoo.com>
2016-02-25 22:22:50 +0800

    ff

--- app/about.php
+++ app/about.php
@@ -24,38 +24,63 @@ require_once 'init.php';?>
              
              
              
-             <!--
-             <div class="six column row">
-                 <div class="column">
-                     <a href="/_/img/about2.jpg" data-lightbox="image1" data-title=""><img src="/_/img/about-store1.jpg"/></a>
+          
+             <div class="six column row" style="padding-left:18px;padding-right:18px">
+                 <div class="column plr0">
+                     <a href="https://static.wixstatic.com/media/aa26f6_c5facc5e833c4b71987af7c5e5458fb7.jpg/v1/fill/w_1067,h_800,al_c,q_90,usm_0.66_1.00_0.01/aa26f6_c5facc5e833c4b71987af7c5e5458fb7.jpg" data-lightbox="image1" data-title=""><img src="https://static.wixstatic.com/media/aa26f6_c5facc5e833c4b71987af7c5e5458fb7.jpg/v1/fill/w_148,h_146,al_c,q_80,usm_0.66_1.00_0.01/aa26f6_c5facc5e833c4b71987af7c5e5458fb7.jpg"/></a>
                  </div>
                  
-                  <div class="column">
-                     <a href="/_/img/about2.jpg" data-lightbox="image1" data-title=""><img src="/_/img/about-store1.jpg"/></a>
+                  <div class="column plr0">
+                     <a href="https://static.wixstatic.com/media/aa26f6_6f5ad4d5a79640fc98f0f67ca9d7ff5d.jpg/v1/fill/w_1067,h_800,al_c,q_90,usm_0.66_1.00_0.01/aa26f6_6f5ad4d5a79640fc98f0f67ca9d7ff5d.jpg" data-lightbox="image1" data-title=""><img src="https://static.wixstatic.com/media/aa26f6_6f5ad4d5a79640fc98f0f67ca9d7ff5d.jpg/v1/fill/w_148,h_146,al_c,q_80,usm_0.66_1.00_0.01/aa26f6_6f5ad4d5a79640fc98f0f67ca9d7ff5d.jpg"/></a>
                  </div>
                  
-                  <div class="column">
-                     <a href="/_/img/about2.jpg" data-lightbox="image1" data-title=""><img src="/_/img/about-store1.jpg"/></a>
+                  <div class="column plr0">
+                     <a href="https://static.wixstatic.com/media/aa26f6_1515f86338b24967bcd6863fa635d6bd.jpg/v1/fill/w_1067,h_800,al_c,q_90,usm_0.66_1.00_0.01/aa26f6_1515f86338b24967bcd6863fa635d6bd.jpg" data-lightbox="image1" data-title=""><img src="https://static.wixstatic.com/media/aa26f6_1515f86338b24967bcd6863fa635d6bd.jpg/v1/fill/w_148,h_146,al_c,q_80,usm_0.66_1.00_0.01/aa26f6_1515f86338b24967bcd6863fa635d6bd.jpg"/></a>
                  </div>
-                  <div class="column">
-                     <a href="/_/img/about2.jpg" data-lightbox="image1" data-title=""><img src="/_/img/about-store1.jpg"/></a>
-                 </div>
-                 
-                  <div class="column">
-                     <a href="/_/img/about2.jpg" data-lightbox="image1" data-title=""><img src="/_/img/about-store1.jpg"/></a>
+                  <div class="column plr0">
+                     <a href="https://static.wixstatic.com/media/aa26f6_d5d1f340b6054d088211bc2cf62e645a.jpg/v1/fill/w_1422,h_800,al_c,q_90,usm_0.66_1.00_0.01/aa26f6_d5d1f340b6054d088211bc2cf62e645a.jpg" data-lightbox="image1" data-title=""><img src="https://static.wixstatic.com/media/aa26f6_d5d1f340b6054d088211bc2cf62e645a.jpg/v1/fill/w_148,h_146,al_c,q_80,usm_0.66_1.00_0.01/aa26f6_d5d1f340b6054d088211bc2cf62e645a.jpg"/></a>
                  </div>
                  
-                  <div class="column">
-                     <a href="/_/img/about2.jpg" data-lightbox="image1" data-title=""><img src="/_/img/about-store1.jpg"/></a>
+                  <div class="column plr0">
+                     <a href="https://static.wixstatic.com/media/aa26f6_1124d3ee47d14c5dba468b0936ff2b90.jpg/v1/fill/w_1422,h_800,al_c,q_90,usm_0.66_1.00_0.01/aa26f6_1124d3ee47d14c5dba468b0936ff2b90.jpg" data-lightbox="image1" data-title=""><img src="https://static.wixstatic.com/media/aa26f6_1124d3ee47d14c5dba468b0936ff2b90.jpg/v1/fill/w_148,h_146,al_c,q_80,usm_0.66_1.00_0.01/aa26f6_1124d3ee47d14c5dba468b0936ff2b90.jpg"/></a>
+                 </div>
+                 
+                  <div class="column plr0">
+                     <a href="https://static.wixstatic.com/media/aa26f6_7c8292d97bf8405f8c7155bee401c70a.jpg/v1/fill/w_1422,h_800,al_c,q_90,usm_0.66_1.00_0.01/aa26f6_7c8292d97bf8405f8c7155bee401c70a.jpg" data-lightbox="image1" data-title=""><img src="https://static.wixstatic.com/media/aa26f6_7c8292d97bf8405f8c7155bee401c70a.jpg/v1/fill/w_148,h_146,al_c,q_80,usm_0.66_1.00_0.01/aa26f6_7c8292d97bf8405f8c7155bee401c70a.jpg"/></a>
                  </div>
                  
              </div>
-             -->
-             <div class="column row nop  "><p class="mt03">全港分店地址： </p></div>
+             
+             <div class="column row nop "><p class="mt03" style="padding-left:15px;">全港分店地址： </p></div>
             
              <div class="three column row nop">
-                <?php echo $system['location'] ?> 
-               
+                 <div class="column">
+                     <p>中环店 : 中环皇后大道中70号卡佛大厦22楼2206-07室<br/>电话 : 2882 6100</p>
+                 </div>
+               <div class="column">
+                   <p>铜锣湾店 : 铜锣湾渣甸街50号渣甸中心16楼全层<br/>电话 : 2567 8444</p>
+                 </div>
+                 <div class="column">
+                     <p>尖沙咀店(1) : 尖沙咀漆咸道南57-59号金马伦中心11楼全层<br/>电话 : 3105 0171</p>
+                 </div>
+                 <div class="column">
+                     <p>尖沙咀店(2) : 尖沙咀金马伦道18-20号万勤商业大厦15楼A室<br/>电话 : 2353 0202</p>
+                 </div>
+                 <div class="column">
+                     <p> 尖沙咀店(3) : 尖沙咀金马伦道18-20号万勤商业大厦15楼B室<br/>电话 :  2414 8444</p>
+                 </div>
+                 <div class="column">
+                     <p>旺角店(1) : 旺角山东街38号38广场5楼A室<br/>电话 :  2155 9986</p>
+                 </div>
+                 <div class="column">
+                     <p>旺角店(2) : 旺角山东街38号38广场5楼B室<br/>电话 :  2890 9300</p>
+                 </div>
+                 <div class="column">
+                     <p> 屯门店 : 屯门栢丽广场15楼1512室<br/>电话 :  2456 3268</p>
+                 </div>
+                 <div class="column">
+                     <p>元朗店 : 元朗大棠路11号光华广场13楼06室<br/>电话 : 2352 0072</p>
+                 </div>
 
 
 
